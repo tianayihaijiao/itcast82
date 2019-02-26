eb50447ce883bbb68c4ebff495ac53ff458a6136
zyk <395557387@qq.com>
2019-02-26 18:31:51 +0800

    111

--- HolloWorld/src/hollo.java
+++ HolloWorld/src/hollo.java
@@ -1,12 +1,5 @@
 public class hollo {
     public static void main(String[] args) {
         System.out.println("HolloWorld");
-		System.out.println("zyk修改了代码);
-		System.out.println("zyk第2次修改了代码);
-		System.out.println("zyk第3次修改了代码);
-		System.out.println("zyk第4次修改了代码);
-		System.out.println("why修改了代码);
-		System.out.println("why第二次修改了代码);
-		System.out.println("why第三次修改了代码);
     }
 }
