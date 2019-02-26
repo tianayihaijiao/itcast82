69f33b752692addfaedcadf0c5f1129092248a7b
weihangyu <408396104@qq.com>
2019-02-26 18:15:22 +0800

    3333

--- HolloWorld/src/hollo.java
+++ HolloWorld/src/hollo.java
@@ -1,7 +1,7 @@
 public class hollo {
     public static void main(String[] args) {
         System.out.println("HolloWorld");
-		System.out.println("zyk修改了代码);
-		System.out.println("zyk第二次修改了代码);
+		System.out.println("why修改了代码);
+		System.out.println("why第二次修改了代码);
     }
 }
